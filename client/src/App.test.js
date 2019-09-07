be984fb62de7bace2d8694da56223ee573fff38f
Sara Alkhamri <s.alkhamri@gmail.com>
2019-09-06 19:57:44 -0700

    test passed. MVP completed

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import * as rtl from '@testing-library/react';
+import Display from './components/Display';
+import PlayesCards from './components/PlayersCard';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+
+// it('renders without crashing', () => {
+//   const div = document.createElement('div');
+//   ReactDOM.render(<App />, div);
+//   ReactDOM.unmountComponentAtNode(div);
+// });
+
+test('containes Name Megan Rapino', async () => {
+  const{findByText} = rtl.render(<App />);
+  findByText(/Megan Rapino/i);
 });
